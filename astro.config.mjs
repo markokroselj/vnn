2e078b11b12ecea24855256af758e5bc074280d5
markokroselj <kroselj.marko123@gmail.com>
2023-08-30 20:27:28 +0200

    Upgraded to Astro 3.0 and added support for mdx files

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
 export default defineConfig({
-    compressHTML: true,
+  compressHTML: true,
+  integrations: [mdx()]
 });
