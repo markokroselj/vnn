8c7cc57533164404b921ead169e521c15fe0fe3e
markokroselj <kroselj.marko123@gmail.com>
2023-09-07 14:30:57 +0200

    added sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
-
 import mdx from "@astrojs/mdx";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://vnn.vercel.app',
   compressHTML: true,
-  integrations: [mdx()]
+  integrations: [mdx(), sitemap()]
 });
